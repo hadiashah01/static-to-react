bcf882267ce2a6a53c7cfcd2cadc4d67dc2a5b56
Hadia <hadiashah010@gmail.com>
2026-07-20 14:59:55 +0500

    import all three components from components/ to main.jsx

--- static-to-react/src/main.jsx
+++ static-to-react/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './components/Header'
+import MainOfBody from './components/MainOfBody'
+import Footer from './components/Footer''
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header></Header>
+    <MainOfBody></MainOfBody>
+    <Footer></Footer>
   </StrictMode>,
 )
