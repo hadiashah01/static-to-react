86cf2e431cb8e03c4693f99555673e15d7d54a96
Hadia <hadiashah010@gmail.com>
2026-07-20 15:15:13 +0500

    Add images in assets

--- static-to-react/src/main.jsx
+++ static-to-react/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
 import MainOfBody from './components/MainOfBody'
-import Footer from './components/Footer''
+import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
